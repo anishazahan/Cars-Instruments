2534c2b4bdec1187f99865e7b7785f22dea0727a
anishazahan <anishazahan13@gmail.com>
2022-05-23 05:00:08 +0600

    navbar-css-style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Components/Pages/Home/Home';
-import Navbar from './Components/Shared/Navbar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Components/Pages/About/About";
+import Blog from "./Components/Pages/Blog/Blog";
+import Home from "./Components/Pages/Home/Home";
+import Services from "./Components/Pages/Services/Services";
+import Navbar from "./Components/Shared/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar>
-       <Home></Home>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="blog" element={<Blog />} />
+         
+        </Routes>
       </Navbar>
-      
     </div>
   );
 }
--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = ({children}) => {
     <div class="drawer drawer-end">
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
-        <div class="w-full navbar bg-base-300">
-          <div class="flex-1 px-2 mx-2">Navbar Title</div>
+        <div class="w-full navbar bg-black px-12">
+          <div class="flex-1 px-2 mx-2 font-bold text-info"> <span className="text-error mr-1.5">CAR</span> MANUFACTURER</div>
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
               <svg
@@ -28,7 +28,33 @@ const Navbar = ({children}) => {
           <div class="flex-none hidden lg:block">
             <ul class="menu menu-horizontal">
               <li>
-                <Link to="/home">Home</Link>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium"  to="/home">Home</Link>
+              </li>
+              <li>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium" to="/about">About</Link>
+              </li>
+              <li>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium" to="/services">Services</Link>
+              </li>
+              <li>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium" to="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium" to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link className="mx-1 rounded-lg text-zinc-400 font-medium" to="login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {children}
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-3" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+        <li>
+                <Link className="rounded-lg" to="/home">Home</Link>
               </li>
               <li>
                 <Link to="/about">About</Link>
@@ -45,20 +71,6 @@ const Navbar = ({children}) => {
               <li>
                 <Link to="login">Login</Link>
               </li>
-            </ul>
-          </div>
-        </div>
-        {children}
-      </div>
-      <div class="drawer-side">
-        <label for="my-drawer-3" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
         </ul>
       </div>
     </div>
