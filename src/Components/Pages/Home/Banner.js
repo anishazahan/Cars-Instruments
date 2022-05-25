4e5359ddf19ba3c0f4ab05ba65f5ecff73fef36f
anishazahan <anishazahan13@gmail.com>
2022-05-25 09:08:43 +0600

    hero-background-img-section

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -14,6 +14,7 @@ const Banner = () => {
             
             
         </div>
+        <div className=""></div>
       
         
     </div>
--- src/Components/Pages/Home/HeroBack.js
+++ src/Components/Pages/Home/HeroBack.js
@@ -1,22 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./HeroBack.css"
 
 const HeroBack = () => {
   return (
-   <div className="background">
+   <div className="background-images">
         <div
       class="hero min-h-screen"
      
     >
-      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-overlay bg-opacity-50"></div>
       <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
-          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-          <p class="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div class="">
+          <h1 class="mb-5 text-2xl font-bold text-info">Industrial Factory</h1>
+          <p class="mb-5 text-5xl font-bold ">The biggest contribution of Ship <br/> industry to Humanity.</p>
+          <p class="mb-5 w-1/2 font-medium mx-auto">
+          Motory Shop offers you a lot of special car-buying services that cover all types of new and used cars in Saudi Arabia with exclusive prices and other free services for our customers.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <Link to="/" class="btn btn-primary">Read More</Link>
         </div>
       </div>
     </div>
